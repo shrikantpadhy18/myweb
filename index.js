6b49c4f06957dc82870575489932ebf61f07e6d1
shrikant padhy <46592561+shrikantpadhy18@users.noreply.github.com>
2019-11-30 21:29:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,4 @@ app.get('/',function(req,res,next)
 }
 );
 
-app.listen(app.get('port'),function(){
-console.log('Express started on http://localhost:'+app.get('port'));
-
-});
+app.listen(process.env.PORT || 3000);
