b40b3d6d4d43ffdbb8a2b6f7ce43b56f3b866e4e
shrikantpadhy <shrikantpadhy18@gmail.com>
2019-12-20 20:30:08 +0530

    changes made

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var handlebars=require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port',process.env.PORT || 3000);
-app.use('/clark',express.static('clark'));
+app.use(express.static(__dirname + '/clark'));
 app.get('/',function(req,res,next)
 {
     res.render(
@@ -20,5 +20,36 @@ app.get('/',function(req,res,next)
     );
 }
 );
+
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/mycustomer",{ useUnifiedTopology: true , useNewUrlParser: true });
+
+var nameSchema = new mongoose.Schema({
+  Name: String,
+    Email: String,
+    Subject:String,
+    Message:String
+    
+});
+var User = mongoose.model("User", nameSchema);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/addname", (req, res) => {
+  var myData = new User(req.body);
+  myData.save()
+    .then(item => {
+      res.send("MESSAGE SENT");
+      
+    })
+    .catch(err => {
+      res.status(400).send("UNABLE TO SENT MESSAGE");
+    });
+});
+
 
 app.listen(process.env.PORT || 3000);
